c7e61f7ad645a5bb316a208f7bdfb25ed62c4c43
vidhisharma3020 <vidhisharma5920@gmail.com>
2024-06-04 17:25:33 +0530

    changes

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const slideshowContainer = document.getElementById("slideshow-container");
     let slideIndex = 0;
 
-    // Function to show the slides
     function showSlides() {
         const slides = document.getElementsByClassName("slide");
         for (let i = 0; i < slides.length; i++) {
@@ -14,17 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
             slideIndex = 1;
         }
         slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
+        setTimeout(showSlides, 2000);
     }
 
-    // Event listener for video end to start slideshow
     video.addEventListener('ended', function() {
         document.querySelector('.video-container').style.display = 'none';
         slideshowContainer.style.display = 'block';
         showSlides();
     });
 
-    // Force end video after it plays once
     video.addEventListener('canplaythrough', function() {
         video.loop = false;
     });
