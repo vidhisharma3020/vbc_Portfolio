3391ba15795838d8997c8d6ead0529dcf1ec46b4
Charishma Bollineni <139740665+Charishma608@users.noreply.github.com>
2024-07-16 12:24:51 +0530

    Update index.js

--- Resources/index.js
+++ Resources/index.js
@@ -1,1 +1,19 @@
+var cursor = document.querySelector(".mymouse")
+document.body.addEventListener("mousemove", function(e){
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
+
+
+var cont = document.querySelector('.fr');
+cont.addEventListener("mouseover", function(){
+  cursor.setAttribute("style", "outline:white solid")
+})
+var cont = document.querySelector('.form-container');
+cont.addEventListener("mouseout", function(){
+  cursor.setAttribute("style", "outline:black solid")
+});
+
+
 
