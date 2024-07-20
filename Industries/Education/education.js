6fee65b458830ebf0a41612885b66180154f6435
vidhisharma3020 <vidhisharma5920@gmail.com>
2024-07-20 21:32:23 +0530

    Navbar links

--- Industries/Education/education.js
+++ Industries/Education/education.js
@@ -4,3 +4,4 @@ const budgetValue = document.getElementById('ctBudgetValue');
 budgetSlider.addEventListener('input', function() {
     budgetValue.textContent = '$' + budgetSlider.value.toLocaleString();
 });
+
